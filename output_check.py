e11bd3c908aee26bcdba3d536dad51c6532a94a3
matthewfschwartz <54508079+matthewfschwartz@users.noreply.github.com>
2021-12-01 15:55:51 -0600

    Update output_check.py

--- output_check.py
+++ output_check.py
@@ -1,5 +1,22 @@
 import pandas as pd
-file = pd.read_excel('file.xlsx')
-file1 = pd.read_excel('file1.xlsx')
-difference = file[file!=file1]
-print(difference)
+import os
+
+cwd = os.getcwd() # Get current working directory
+print(cwd)
+# DataFrame in pandas == Excel worksheet
+path1 = os.path.join(cwd, "file1.xlsx")
+path2 = os.path.join(cwd, "file2.xlsx")
+df1 = pd.read_excel(path1)
+df2 = pd.read_excel(path2)
+
+print(df1.equals(df2))
+
+diff_count = 0
+columns = list(df1) # Create a list that holds the contents of each column
+num_rows = len(df1.index) # Find the number of rows that each excel sheet has
+
+for i in columns:
+    for j in range(num_rows):
+        if df1[i][j] != df2[i][j]: # compare sheet one to sheet two
+            diff_count += 1
+print(diff_count)
