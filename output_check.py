c0bc970050b87f13b1043a9c599ef079e4f33e70
matthewfschwartz <54508079+matthewfschwartz@users.noreply.github.com>
2021-12-02 17:18:44 -0600

    Update output_check.py

--- output_check.py
+++ output_check.py
@@ -1,15 +1,19 @@
 import pandas as pd
+import numpy as np
 import os
 
 cwd = os.getcwd() # Get current working directory
 print(cwd)
 # DataFrame in pandas == Excel worksheet
-path1 = os.path.join(cwd, "file1.xlsx")
-path2 = os.path.join(cwd, "file2.xlsx")
-df1 = pd.read_excel(path1)
-df2 = pd.read_excel(path2)
+path1 = os.path.join(cwd, "file3.csv")
+path2 = os.path.join(cwd, "file4.csv")
+#df1 = pd.read_csv(path1.read())
+#df2 = pd.read_csv(path2.read())
+df1 = pd.read_csv(path1)
+df2 = pd.read_csv(path2)
 
 print(df1.equals(df2))
+#print(np.isClose(df1, df2))
 
 diff_count = 0
 columns = list(df1) # Create a list that holds the contents of each column
@@ -20,3 +24,4 @@ for i in columns:
         if df1[i][j] != df2[i][j]: # compare sheet one to sheet two
             diff_count += 1
 print(diff_count)
+
